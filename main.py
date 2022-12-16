9b589625deda325fc762bce14857c8faea857f38
seanstephens17 <d00211442@student.dkit.ie>
2022-12-16 18:21:41 +0000

    Attempting to read in tiny images folder

--- main.py
+++ main.py
@@ -1,14 +1,80 @@
 import cv2 as cv
 import numpy as np
+import os
+import random
+import pickle
 import matplotlib.pyplot as plt
 from tensorflow.keras import datasets, layers, models
 
-(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+DATADIR = "C:\Users\Sean Stephens\OneDrive\Desktop\tiny-imagenet-200"
+CATEGORIES = ['', '']
+
+for category in CATEGORIES:
+    path = os.path.join(DATADIR, category)
+    for img in os.listdir(path):
+        img_array = cv.imread(os.path.join(path, img), cv.IMREAD_GRAYSCALE)
+        plt.imshow(img_array, cmap='gray')
+        plt.show()
+        break
+    break
+
+IMG_SIZE = 64
+new_array = cv.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap='gray')
+plt.matshow()
+
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category)
+        class_num = CATEGORIES.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv.imread(os.path.join(path, img), cv.IMREAD_GRAYSCALE)
+                new_array = cv.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array, class_num])
+            except Exception as e:
+                pass
+
+
+create_training_data()
+print(len(training_data))
+random.shuffle(training_data)
+for sample in training_data[:10]:
+    print(sample[1])
+
+X = []
+y = []
+
+for features, label in training_data:
+    X.append(features)
+    y.append(label)
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+pickle_out = open("X.pickle", "wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle", "wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
+X[1]
+
+pickle_in = open("y.pickle", "rb")
+y = pickle.load(pickle_in)
+y[1]
+
+(training_images, training_labels), (testing_images, testing_labels) = datasets.load_data("C:\Users\Sean Stephens\OneDrive\Desktop\tiny-imagenet-200\train\n01443537\images")
 training_images, testing_images = training_images / 255, testing_images / 255
 
-class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+class_names = ['goldfish']
 
-for i in range(16):
+for i in range(500):
     plt.subplot(4, 4, i + 1)
     plt.xticks([])
     plt.yticks([])
@@ -23,7 +89,7 @@ testing_images = testing_images[:4000]
 testing_labels = testing_labels[:4000]
 
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(64, 64, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -38,3 +104,9 @@ model.fit(training_images, training_labels, epochs=10, validation_data=(testing_
 loss, accuracy = model.evaluate(testing_images, testing_labels)
 print(f"loss: {loss}")
 print(f"accuracy: {accuracy}")
+
+model.save('image_classifier.model')
+
+# model = models.load_model('image_classifier.model')
+
+# img = cv.imread
